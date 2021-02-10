19bc79cdb72d9cf81a3801e8f5c689166c3f7b12
Ashutosh Singh <thecodepapaya@gmail.com>
2021-02-10 21:29:07 +0530

    Add server_key validation

--- attendance/views.py
+++ attendance/views.py
@@ -2,8 +2,8 @@ from django.http import Http404, HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from meta.models import MetaData
-from registration.models import Registration
 from meta.serializers import MetaSerializer
+from registration.models import Registration
 from rest_framework import status
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
@@ -22,12 +22,15 @@ class attendance_list(APIView):
     def post(self, request, format=None):
         serializer = AttendanceSerializer(data=request.data)
         if serializer.is_valid():
+            email = serializer.validated_data['student_data'].institute_email
             meta_data = MetaData.objects.all()[0]
-            registration_data = Registration.objects.get()[0]
+            registration_data = Registration.objects.filter(
+                student_data__institute_email=email)[0]
             if serializer.validated_data['app_build_number'] < meta_data.min_app_build:
                 return Response(MetaSerializer(meta_data).data, status=status.HTTP_403_FORBIDDEN)
-            if serializer.validated_data['server_key'] != Registration.objects.get_object():
-                return Response(MetaSerializer(meta_data).data, status=status.HTTP_403_FORBIDDEN)
+            # If registration was invalidated or server_key is not correct
+            if serializer.validated_data['server_key'] != registration_data.server_key:
+                return Response({'details': 'Your registeration has been invalidated, register again'}, status=status.HTTP_403_FORBIDDEN)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
