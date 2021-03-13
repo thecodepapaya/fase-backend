c27248743d6e43a24857cc590c346d7be4192a67
Ashutosh Singh <thecodepapaya@gmail.com>
2021-03-14 00:13:37 +0530

    Increase email name guid length in faculty and student models, make migrations

--- students/models.py
+++ students/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class StudentData(models.Model):
-    google_uid = models.CharField(max_length=15)
-    institute_email = models.EmailField(max_length=28, primary_key=True)
-    name = models.CharField(max_length=35)
+    google_uid = models.CharField(max_length=28)
+    institute_email = models.EmailField(max_length=40, primary_key=True)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return 'Name: '+self.name+'\nEmail: '+self.institute_email+'\nGoogle login ID: '+self.google_uid
