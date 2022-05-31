1ce6f5a573cdfe3ff6e29fb126714ba5221cedc7
Ashutosh Singh <thecodepapaya@gmail.com>
2022-05-31 13:56:40 +0530

    Simplify login logic

--- authentication/views.py
+++ authentication/views.py
@@ -1,18 +1,13 @@
-import token
-import jwt
-from rest_framework import viewsets
-
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import permissions
 
 from authentication.firebase import decode_token
-from .permissions import SkipAuth
 from users.models import User
 from users.serializers import UserSerializer
 
 
-@permission_classes([SkipAuth])
+@permission_classes([permissions.AllowAny])
 @api_view(http_method_names=['POST', ])
 def login(request):
     token_header = request.headers.get('Authorization')
@@ -26,31 +21,14 @@ def login(request):
     if not decoded_token:
         return Response(status=401)
 
-    user, response_code = _get_or_create_user(decoded_token)
-    serializer = UserSerializer(user)
-
-    return Response(data=serializer.data, status=response_code)
-
-
-def _get_or_create_user(decoded_token):
     email = decoded_token.get('email')
     name = decoded_token.get('name')
     picture = decoded_token.get('picture', None)
-    response_code = 200
 
-    try:
-        user = User.objects.get(pk=email)
-        print(f'Get user: {user}')
-    except User.DoesNotExist:
-        user = _create_user(email, name, picture)
-        print(f'New user: {user}')
-        response_code = 201
+    user, is_created = User.objects.get_or_create(
+        pk=email, defaults={'name': name, 'display_picture': picture})
+    response_code = 201 if is_created else 200
 
-    return user, response_code
+    serializer = UserSerializer(user)
 
-
-def _create_user(email, name, picture):
-    user = User(institute_email=email, name=name, display_picture=picture)
-    user.save()
-
-    return user
+    return Response(data=serializer.data, status=response_code)
