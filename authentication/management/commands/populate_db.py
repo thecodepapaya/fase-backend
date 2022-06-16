fdd33d0f6dbda33febf310d3e5ab07d4dc1e6a83
Ashutosh Singh <thecodepapaya@gmail.com>
2022-06-16 22:00:37 +0530

    Add download link db to populate script

--- authentication/management/commands/populate_db.py
+++ authentication/management/commands/populate_db.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import Group
-from meta.models import MetaData
+from meta.models import MetaData, DownloadLink
 
 
 class Command(BaseCommand):
@@ -8,13 +8,37 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        print('Creating Permission Groups')
-        faculty_group = Group.objects.get_or_create(name='Faculty')
-        student_group = Group.objects.get_or_create(name='Student')
+        try:
+            print('Creating Permission Groups')
+            Group.objects.get_or_create(name='Faculty')
+            Group.objects.get_or_create(name='Student')
+            self.stdout.write(self.style.SUCCESS(
+                'Created permission group Faculty and Student. Please add appropriate permission to the groups in admin panel.'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(
+                f'Failed to create groups. Error {e}'))
 
-        meta_data = MetaData.objects.create(
-            min_app_build=6,
-            min_app_version='1.2.1',
-        )
+        try:
+            MetaData.objects.get_or_create(
+                min_app_build=6,
+                min_app_version='1.2.1',
+            )
+            self.stdout.write(self.style.SUCCESS(
+                'Added application version metadata.'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(
+                f'Failed to add MetaData. Error {e}.'))
+
+        try:
+
+            DownloadLink.objects.get_or_create(
+                apk_download='https://fase.thecodepapaya.dev',
+                ios_download='https://fase.thecodepapaya.dev',
+            )
+            self.stdout.write(self.style.SUCCESS(
+                f'Added default download links.'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(
+                f'Failed to add download links. Error {e}.'))
 
         self.stdout.write(self.style.SUCCESS('Success'))
